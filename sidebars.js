cbb1bcf3089aa7232a8ee188aa12e93833ff4385
sgdwn <74820599+sgdwn@users.noreply.github.com>
2021-03-13 16:25:37 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,5 @@
 module.exports = {
   someSidebar: {
-    'Introduction': ['doc1']
-    'Biological Molecules': ['doc2', 'doc3']
     'Introduction': ['doc1'],
     'Biological Molecules': ['doc2', 'doc3'],
     'Cells': ['mdx'],
