48433b3ee2c1de49eeda435cd43894c14d469d00
sgdwn <74820599+sgdwn@users.noreply.github.com>
2021-03-13 14:59:04 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
-    'Introduction': ['doc1']
-    'Biological Molecules': ['doc2', 'doc3']
+    'Introduction': ['doc1'],
+    'Biological Molecules': ['doc2', 'doc3'],
     'Cells': ['mdx'],
   },
 };
