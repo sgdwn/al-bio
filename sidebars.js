ae26e27d4a8a788f9dd94d64b96f61a6c49784bd
sgdwn <74820599+sgdwn@users.noreply.github.com>
2021-03-13 16:24:53 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,11 +1,9 @@
 module.exports = {
   someSidebar: {
+    'Introduction': ['doc1']
+    'Biological Molecules': ['doc2', 'doc3']
     'Introduction': ['doc1'],
-    'Biological Molecules': ['doc2', type SidebarItemLink = {
-  type: 'link',
-  label: 'Summary PDF',
-  href: 'https://github.com/sgdwn/doc_assets/blob/157570dcb2adef65b43e542656f0046738ec2609/3.1%20Biological%20Molecules%20Summary.pdf';
-};],
+    'Biological Molecules': ['doc2', 'doc3'],
     'Cells': ['mdx'],
   },
 };
